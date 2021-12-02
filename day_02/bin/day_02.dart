69a0b2784c9db662d71a572615a713ff0f75a356
Jonas Neubürger <jonas793@gmail.com>
2021-12-02 14:27:54 +0100

    finish part 1 and add part 2 class

--- day_02/bin/day_02.dart
+++ day_02/bin/day_02.dart
@@ -1,3 +1,19 @@
+/*
+https://adventofcode.com/2021/day/1
+*/
+
+import 'parts.dart';
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  List<String> linesString = File('./input/raw_input.txt').readAsLinesSync();
+  var partOne = PartOne(linesString);
+  partOne.calculate();
+  var solutionPartOne = partOne.solution;
+  print('Solution of Part 1: $solutionPartOne\n -----------------------------');
+
+  var partTwo = PartTwo(linesString);
+  partOne.calculate();
+  var solutionPartTwo = partTwo.solution;
+  print('Solution of Part 2: $solutionPartTwo\n -----------------------------');
 }
