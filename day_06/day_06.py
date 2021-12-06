b152063a25d39ebccc6bcfc44d201f8350c9f5e4
Jonas Neubürger <jonas793@gmail.com>
2021-12-06 12:46:25 +0100

    finish part 2

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -27,9 +27,8 @@ class lanternfish:
             self.internal_timer -=1
             self.age += 1
             return None
-        
 
-def test():
+def test(duration):
     test_data = [3,4,3,1,2]
     # create fish array
     fish_array = []
@@ -38,21 +37,15 @@ def test():
         fish_array.append(lanternfish(i, timer, 0))
         i+=1
     
-    fish_timers = [x.internal_timer for x in fish_array]
-    print('Initial state: {}'.format(fish_timers))
-
-    duration = 18
-    
     for i in range(1,duration+1):
         for bert in fish_array:
             new_fish = bert.next_day()
             if new_fish:
                 fish_array.append(new_fish)
-        fish_timers = [x.internal_timer for x in fish_array]
-        print('After {} days: {}'.format(i, fish_timers))
+    print('After {} days there are {} lanternfish'.format(duration, len(fish_array)))    
     
 
-def part1():
+def calculate(duration):
     # read initial state
     with open('input/raw_input.txt') as f:
         data = f.readline().split(',')
@@ -66,8 +59,6 @@ def part1():
     for timer in data:
         fish_array.append(lanternfish(i, timer, 0))
         i+=1
-
-    duration = 80
     
     for i in range(1,duration+1):
         for bert in fish_array:
@@ -75,11 +66,53 @@ def part1():
             if new_fish:
                 fish_array.append(new_fish)
     print('After {} days there are {} lanternfish'.format(duration, len(fish_array)))
-    print('Last fish was: {}'.format(fish_array[-1].name))
+
+
+def new_fish(st, d):
+        fish_array = [st.count(i) for i in range(7)]
+        pufferfish = [0, 0]
+        # calculate the fish growth
+        for i in range(d):
+            daddy = pufferfish.pop(0)
+            kid = fish_array.pop(0)
+            pufferfish.append(kid)
+            fish_array.append(kid + daddy)
+        return sum(fish_array)+sum(pufferfish)
+
+def test_part2(test_data,duration):      
+    # calculate the fish growth
+    return new_fish(test_data, duration)
+
+def calculate_part2(data, duration):    
+    return new_fish(data, duration)
+
 
 if __name__ == '__main__':
-    #test()
+    '''
     start_time = time.perf_counter()
-    part1()
+    test(200)
+    finish_time = time.perf_counter()
+    print(f"Calculated test in {finish_time - start_time:0.4f} seconds")
+    
+    start_time = time.perf_counter()
+    calculate(80)
     finish_time = time.perf_counter()
     print(f"Calculated part 1 in {finish_time - start_time:0.4f} seconds")
+    '''
+
+    with open('input/raw_input.txt') as f:
+        data = f.readline().split(',')
+    data = [int(x) for x in data]
+    
+    start_time = time.perf_counter()    
+    test_data = [3,4,3,1,2]
+    res = test_part2(test_data, 80)   
+    finish_time = time.perf_counter()
+    print('After {} days there are {} lanternfish'.format(80, res)) 
+    print(f"Calculated part 2 in {finish_time - start_time:0.4f} seconds")
+
+    start_time = time.perf_counter()
+    res = calculate_part2(data, 256)
+    finish_time = time.perf_counter()
+    print('After {} days there are {} lanternfish'.format(256, res)) 
+    print(f"Calculated part 2 in {finish_time - start_time:0.4f} seconds")
