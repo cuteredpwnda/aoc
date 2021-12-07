353ad601fd792ebee9dcef8bc9c5cb5c6c41b715
Jonas Neubürger <jonas793@gmail.com>
2021-12-07 08:06:04 +0100

    add dummies for day 4 and 5

---
+++ day_04/README.md
@@ -0,0 +1,7 @@
+# [Day 7](https://adventofcode.com/2021/day/6)
+## What have I done?
+### Step 1 (Prerequisites)
+I have used curl to get the input with `curl "https://adventofcode.com/2021/day/7/input" --cookie "session=***" -o raw_input.txt`, `***` being the session cookie ofc.
+
+### Step 2 (Part 1)
+### Step 3 (Part 2)
---
+++ day_05/day_05.py
@@ -0,0 +1,47 @@
+import numpy as np
+import time
+
+# create a fish with (name, internal_timer, age)
+# initial value of internal timer at creation is 2 
+
+def test():
+    return 0
+
+def calculate(data):
+    return 0
+
+
+def test_part2():      
+    return 0
+
+def calculate_part2(data):    
+    return 0
+
+
+if __name__ == '__main__':    
+    
+    
+    start_time = time.perf_counter()
+    test()
+    finish_time = time.perf_counter()
+    print(f"Calculated test in {finish_time - start_time:0.4f} seconds")
+    
+    with open('input/raw_input.txt') as f:
+        data = f.readline().split(',')
+    data = [int(x) for x in data]
+    
+    
+    start_time = time.perf_counter()
+    calculate(data)
+    finish_time = time.perf_counter()
+    print(f"Calculated part 1 in {finish_time - start_time:0.4f} seconds")
+    
+    start_time = time.perf_counter() 
+    res = test_part2()   
+    finish_time = time.perf_counter()
+    print(f"Calculated part 2 in {finish_time - start_time:0.4f} seconds")
+    
+    start_time = time.perf_counter()
+    res = calculate_part2(data)
+    finish_time = time.perf_counter()
+    print(f"Calculated part 2 in {finish_time - start_time:0.4f} seconds")
