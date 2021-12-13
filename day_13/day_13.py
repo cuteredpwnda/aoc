4482a522e55255d15cb80d0251a9f51b86bb3806
Jonas Neubürger <jonas793@gmail.com>
2021-12-13 12:25:40 +0100

    fix wrong folding resulting in folding again after result is calculated

--- day_13/day_13.py
+++ day_13/day_13.py
@@ -9,22 +9,16 @@ def part1(b, stop_at_one=True):
         b.remove('')
     points = [(int(s[0]), int(s[1])) for s in [elem.split(',') for elem in b if 'fold along' not in elem]]
     fold_instruction =  [elem for elem in b if 'fold along' in elem]
-    #print('Points (x,y): ', points)
-    #print('Where to fold (x,y): ', fold_instruction)
 
     max_y = max([y for y in [val[1] for val in points]])
     max_x = max([x for x in [val[0] for val in points]])
 
-    #print(max_x, max_y)
-    # create a matrix(y, x):
     m = np.zeros((max_y+1, max_x+1), dtype=str)
     m[m==''] = '.'
 
     for pt in points:
         m[pt[1], pt[0]] = '#'
 
-    #print(m)
-    
     # now fold
     i = 1
     for fold_ins in fold_instruction:
@@ -35,17 +29,13 @@ def part1(b, stop_at_one=True):
             # "invert" bottom
             bottom_inv = np.flipud(bottom)
             m = np.where(bottom_inv != '.', bottom_inv, top)
-            #print('Result of folding: \n', m)
         if fold_ins.split('=')[0][-1] == 'x':
             left = m[:, line_to_fold+1:]
             right = m[:, :line_to_fold]
-            # "invert" right
-            right_inv = np.fliplr(right)
-            #print('Bottom part inv:\n',bottom_inv)
-            m = np.where(right_inv != '.', right_inv, left)
-            #print('Result of folding: \n', m)
+            # "invert" left
+            left_inv = np.fliplr(left)
+            m = np.where(left_inv != '.', left_inv, right)
         dot_count = (m == '#').sum()
-        print('Dots visible after fold {}: {}'.format(i, dot_count))
         i+=1
         if stop_at_one:
             break
@@ -55,7 +45,6 @@ def part2(b):
     m, res = part1(b, stop_at_one=False)
     f = open("output.txt", "w")
     m[m =='#'] =  u"\u2588"
-    m = np.fliplr(m)
     for line in range(0, len(m)):
         lstr = ''.join(m[line, :])
         print(lstr)
@@ -65,13 +54,13 @@ def part2(b):
 
 def test():
     out = read_input(file='input/test_input.txt')
-    print('Result for part 1: ', part1(input_matrix))
-    #print(part2(out))
-    #print('Result for part 2: ', res2, 'Matrix: \n', m2)
+    _, res = part1(out)
+    print('Result for part 1 in test: ', res)    
+    part2(out)
 
 if __name__ == '__main__':
     input_matrix = read_input()
-    #test()
+    test()
     m, res = part1(input_matrix)
     print('Result for part 1: ', res)
     part2(input_matrix)
