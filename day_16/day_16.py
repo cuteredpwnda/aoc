90e00df5ede6c9e01dc3df6a9c3117503ddd14ed
Jonas Neubürger <jonas793@gmail.com>
2021-12-16 17:31:57 +0100

    finish part 1

--- day_16/day_16.py
+++ day_16/day_16.py
@@ -18,7 +18,7 @@ def read_input(path_to_file:str = 'input/raw_input.txt') -> str:
     """
 
     with open(path_to_file) as f:
-        data = f.readlines()[0]
+        data = f.readlines()[0].strip('\n')
     return data
 
 HEX_TO_BIN_DICT:dict = {'0' : '0000',
@@ -163,14 +163,14 @@ def test():
     '''
     
 if __name__ == '__main__':
-    input_matrix = read_input()
-    test()
-    '''
+    data = read_input()
+    #test()
+    
     start_time = time.perf_counter()
-    print('\nResult for part 1: ', part1(input_matrix))
+    print('\nResult for part 1: ', part1(data))
     finish_time = time.perf_counter()
     print(f"Calculated part 1 in {(finish_time - start_time):0.4f}s")
-    
+    '''
     start_time = time.perf_counter()
     print('\nResult for part 2: ', part2(input_matrix))
     finish_time = time.perf_counter()
