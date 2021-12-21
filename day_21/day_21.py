a8c78b5142645eaf27b8926b2e0482610f1a4a3f
Jonas Neubürger <jonas793@gmail.com>
2021-12-21 14:13:40 +0100

    start part 2 with dynamic programming approach

--- day_21/day_21.py
+++ day_21/day_21.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 import numpy as np
-from itertools import cycle, repeat
+from itertools import cycle, permutations
 
 def read_input(path_to_file:str = 'input/raw_input.txt') -> str:
     with open(path_to_file) as f:
@@ -9,7 +9,7 @@ def read_input(path_to_file:str = 'input/raw_input.txt') -> str:
     return data
 
 
-def part1(data, dice_label_range=100, win_condition = 1000):
+def part1(data, dice_label_range=100, win_condition = 1000, part_two = False):
     # init everything
     player_one_start = int([x.split(': ') for x in data][0][1])
     player_two_start = int([x.split(': ') for x in data][-1][1])
@@ -17,42 +17,73 @@ def part1(data, dice_label_range=100, win_condition = 1000):
     det_dice = cycle([x for x in range(1,dice_label_range+1)])
     player_scores = {   0: {'name': player_list[0],
                             'curr_pos': player_one_start, 
-                            'score': 0},
+                            'score': 0,
+                            'won': 0},
                         1: {'name': player_list[-1],
                             'curr_pos': player_two_start, 
-                            'score': 0}}
-    def game():
+                            'score': 0,
+                            'won': 0}}
+    STATE = dict()
+    def game(alt = part_two):
         iterator = 0
         curr_player_id = 0
-        while (1):
-            iterator+=3
-            # calculate the dice throws
-            moves = []     
-            for _ in range(0,3):
-                moves.append(next(det_dice))
+        if alt: # for part 2, splitting into universes
+            # 10 possible permutations for each player resulting in 21 positions for each (because end score is 21)
+            def calc_universes(score_dict):
+                if score_dict[curr_player_id]['score'] >= win_condition: # checks if anyone won
+                    score_dict[curr_player_id]['score'] += 1
+                    return score_dict
+                if STATE.get(score_dict): # checks if this universe already exists
+                    return STATE[score_dict]
+                for first in range(1,dice_label_range+1):
+                    for second in range(1,dice_label_range+1):
+                        for third in range(1,dice_label_range+1):
+                            # calculate the score like in pt 1
+                            score_dict[score_dict]['curr_pos'] += (first+second+third)%10
+                            if score_dict[curr_player_id]['curr_pos'] > 10:
+                                score_dict[curr_player_id]['curr_pos'] = score_dict[curr_player_id]['curr_pos']%10
+                            score_dict[curr_player_id]['score'] += score_dict[curr_player_id]['curr_pos']
 
-            # get next position
-            player_scores[curr_player_id]['curr_pos'] += sum(moves)%10
-            if player_scores[curr_player_id]['curr_pos'] > 10:
-                player_scores[curr_player_id]['curr_pos'] = player_scores[curr_player_id]['curr_pos']%10
+                            # recurse down
+                            new_player_scores = calc_universes(score_dict)
+                            
+                # set new calculated state in STATE dictionary
+                STATE[score_dict] = new_player_scores
+                return new_player_scores
 
-            # increase the score
-            player_scores[curr_player_id]['score'] += player_scores[curr_player_id]['curr_pos']
-            # print('{} rolls {} and moves to space {} for a total score of {}'.format(player_scores[curr_player_id]['name'], '+'.join([str(x) for x in moves]), player_scores[curr_player_id]['curr_pos'], player_scores[curr_player_id]['score']))
-            # check for winner
-            if (player_scores[curr_player_id]['score'] >= win_condition):
-                other_player_id = 0 if curr_player_id == 1 else 1
-                return player_scores, iterator*player_scores[other_player_id]['score']
+            # start with default values
+            print(player_scores)
+            return calc_universes(player_scores)
 
-            # iterators
-            curr_player_id %= 2
-            curr_player_id = 0 if curr_player_id == 1 else 1
+        else: # for part 1
+            while (1):
+                iterator+=3
+                # calculate the dice throws
+                moves = []
+                for _ in range(0,3):
+                    moves.append(next(det_dice))
+                # get next position
+                player_scores[curr_player_id]['curr_pos'] += sum(moves)%10
+                if player_scores[curr_player_id]['curr_pos'] > 10:
+                    player_scores[curr_player_id]['curr_pos'] = player_scores[curr_player_id]['curr_pos']%10
+
+                # increase the score
+                player_scores[curr_player_id]['score'] += player_scores[curr_player_id]['curr_pos']
+                # print('{} rolls {} and moves to space {} for a total score of {}'.format(player_scores[curr_player_id]['name'], '+'.join([str(x) for x in moves]), player_scores[curr_player_id]['curr_pos'], player_scores[curr_player_id]['score']))
+                # check for winner
+                if (player_scores[curr_player_id]['score'] >= win_condition):
+                    other_player_id = 0 if curr_player_id == 1 else 1
+                    return player_scores, iterator*player_scores[other_player_id]['score']
+
+                # iterators
+                curr_player_id %= 2
+                curr_player_id = 0 if curr_player_id == 1 else 1
 
     return game()
 
 
 def part2(data):
-    player_scores, result = part1(data, dice_label_range=3)
+    player_scores, result = part1(data, dice_label_range=3, win_condition=21, part_two=True)
     return 0
 
 
