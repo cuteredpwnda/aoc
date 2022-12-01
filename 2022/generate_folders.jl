f62d508e1661b5b2a16769896130a60177268b28
Jonas Neubürger <jonas793@gmail.com>
2022-12-01 07:46:00 +0100

    fix downloading and add next day

--- 2022/generate_folders.jl
+++ 2022/generate_folders.jl
@@ -2,9 +2,7 @@ using Dates
 using DotEnv
 DotEnv.config(path=joinpath(@__DIR__, "..", ".env"))
 
-baseurl = "https://adventofcode.com/2022/day"
 function padDate(date::Int64)
-    println("$date, date<10: $(date<10)")
     if date < 10
         return "0$date"
     elseif date > 25
@@ -15,36 +13,48 @@ function padDate(date::Int64)
 end
 
 today = Dates.day(now())
+tomorrow = today + 1
 day_pad = padDate(today)
+tomorrow_pad = padDate(tomorrow)
 
-
-if today === nothing
-    day_pad = "01"
-    baseurl = "https://adventofcode.com/2021/day"
-elseif today > 25
-    day_pad = "25"
-    baseurl = "https://adventofcode.com/2021/day"
-end
-
-templates = joinpath(@__DIR__, "templates")
-if !isnothing((day_pad))
-    folder_template = "day_$day_pad"
-    if !isdir(folder_template)
-        # make folder
-        mkpath(folder_template)
-        # Copy template files
-        cp("$templates/day_xx.jl", joinpath(folder_template, "day_$day_pad.jl"))
+days_to_generate = [day_pad, tomorrow_pad]
+for day in days_to_generate
+    if day === nothing
+        pad = "01"
+    elseif parse(Int64, day) > 25
+        pad = "25"
+        baseurl = "https://adventofcode.com/2021/day"
     else
-        println("Folder already exists.")
+        pad = day
+        baseurl = "https://adventofcode.com/2022/day"
     end
-    if !isdir(joinpath(folder_template, "input"))
-        # get input
-        mkpath(joinpath(folder_template, "input"))
-        url = "$baseurl/$day_pad/input"
-        cookie = ENV["AOC_COOKIE"]
-        # get input
-        run(`curl $url --output $folder_template/input/input.txt --cookie "session=$cookie"`)
+
+
+    templates = joinpath(@__DIR__, "templates")
+    if !isnothing((pad))
+        folder_template = "day_$pad"
+        if !isdir(folder_template)
+            # make folder
+            mkpath(folder_template)
+            # Copy template files
+            cp("$templates/day_xx.jl", joinpath(folder_template, "day_$pad.jl"))
+        else
+            println("Folder for day$pad already exists.")
+        end
+        if parse(Int64, pad) == today
+            if !isdir(joinpath(folder_template, "input"))
+                mkpath(joinpath(folder_template, "input"))
+            end
+            url = "$baseurl/$(parse(Int64, pad))/input"
+            cookie = ENV["AOC_COOKIE"]
+            # get input
+            if !isfile(joinpath(folder_template, "input", "input.txt"))
+                run(`curl -s $url --output $folder_template/input/input.txt --cookie "session=$cookie"`)
+            else
+                println("Input already exists.")
+            end
+        end
+    else
+        println("No folder created, day ($pad) is not in range")
     end
-else
-    println("No folder created, day ($day_pad) is not in range")
 end
