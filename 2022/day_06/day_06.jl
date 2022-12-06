07045b1b9dab22a287985f43c71d89182451a9a4
Jonas Neubürger <jonas793@gmail.com>
2022-12-06 06:57:40 +0100

    condense code

--- 2022/day_06/day_06.jl
+++ 2022/day_06/day_06.jl
@@ -1,23 +1,19 @@
 # Part 1
 function part1(input)
-    for (i, char) in enumerate(input[1])
-        window = input[1][i:i+3]
-        unique_chars = Set(window)
+    for i in eachindex(input[1])
+        unique_chars = Set(input[1][i:i+3])
         if length(unique_chars) == 4
-            signal_start = i+3
-            return signal_start
+            return i+3
         end
     end
 end
 
 # Part 2
 function part2(input)
-    for (i, char) in enumerate(input[1])
-        window = input[1][i:i+13]
-        unique_chars = Set(window)
+    for i in eachindex(input[1])
+        unique_chars = Set(input[1][i:i+13])
         if length(unique_chars) == 14
-            signal_start = i+13
-            return signal_start
+            return i+13
         end
     end
 end
