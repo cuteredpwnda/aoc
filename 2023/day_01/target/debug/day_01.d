40d8cd4da7f8cd228ca29dfe68962ce5060e4d2f
Jonas Neubürger <jonas793@gmail.com>
2023-12-03 16:22:44 +0100

    fix 2023

--- 2023/day_01/target/debug/day_01.d
+++
@@ -1,1 +0,0 @@
-/mnt/g/Dokumente/Programmieren/Advent\ of\ Code/2023/day_01/target/debug/day_01: /mnt/g/Dokumente/Programmieren/Advent\ of\ Code/2023/day_01/src/main.rs
--- 2023/day_01/target/debug/deps/day_01-1568c20ddadc73e9.d
+++
@@ -1,5 +0,0 @@
-/mnt/g/Dokumente/Programmieren/Advent of Code/2023/day_01/target/debug/deps/day_01-1568c20ddadc73e9: src/main.rs
-
-/mnt/g/Dokumente/Programmieren/Advent of Code/2023/day_01/target/debug/deps/day_01-1568c20ddadc73e9.d: src/main.rs
-
-src/main.rs:
