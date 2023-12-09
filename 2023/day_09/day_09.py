3eeea4afd57e95423d97004e9a7b3a26d36ebdac
Jonas Neubürger <jonas793@gmail.com>
2023-12-09 11:41:58 +0100

    finish part 2 and add a cool print 🖨️

--- 2023/day_09/day_09.py
+++ 2023/day_09/day_09.py
@@ -39,39 +39,57 @@ def get_sequences(line):
         extrapolate_set.append(sequences)
     return extrapolate_set
 
-def extrapolate(extrapolate_set):
-    # add a zero to the last sequence
+def extrapolate(extrapolate_set, pt_2=False, test=False):
+    if pt_2: extrapolate_set = [s[::-1] for s in extrapolate_set]
+    # add a zero to the last sequence    
     extrapolate_set[-1].append(0)
     extrapolate_set = extrapolate_set[::-1]
     # extrapolate up to the first sequence
-    finish_len = len(extrapolate_set[-1])+1
+    finish_len = len(extrapolate_set[-1]) + 1
     for i in range(len(extrapolate_set)):
         if len(extrapolate_set[i]) == finish_len:
             break
         # add the sum of the last number of the sequence before and the current last number of the sequence
         curr_last = extrapolate_set[i][-1]
         prev_last = extrapolate_set[i+1][-1]
-        extrapolate_set[i+1].append(np.sum([curr_last, prev_last]))
+        if not pt_2:
+            extrapolate_set[i+1].append(np.sum([curr_last, prev_last]))
+        else:
+            extrapolate_set[i+1].append(prev_last - curr_last)
+    if pt_2: extrapolate_set = [s[::-1] for s in extrapolate_set]
+    if test: cool_print(extrapolate_set)
     return extrapolate_set
 
+def cool_print(extrapolate_set):
+    print_set = extrapolate_set[::-1]
+    for i in range(len(extrapolate_set)):
+        num = print_set[i]
+        # pad num to be the same length as the longest number
+        num = [str(x).rjust(3, " ") for x in num]
+        # insert i*2 spaces before the sequence
+        print("  "*i+f"{num}".replace("[", "").replace("]", "").replace(",", "").replace("'", ""))
+
 @timing
-def pt1(input):
-    last_elements = [extrapolate(get_sequences(x))[-1][-1] for x in input]
+def pt1(input, test=False):
+    last_elements = [extrapolate(get_sequences(x), test)[-1][-1] for x in input]
     print(f"Result Part 1: {np.sum(last_elements)}")
 
 @timing
-def pt2(input):
-    print(f"Result Part 2: {None}")
+def pt2(input, test=False):
+    last_elements = [extrapolate(get_sequences(x), True, test)[-1][0] for x in input]
+    print(f"Result Part 2: {np.sum(last_elements)}")
 
 
 def main():
     test_input_file = glob(os.path.join(os.path.dirname(__file__), "input", "test.txt"))[0]
     input_file = glob(os.path.join(os.path.dirname(__file__), "input", "input.txt"))[0]
     # pt 1
-    pt1(read_input(test_input_file))
+    pt1(read_input(test_input_file), True)
     pt1(read_input(input_file))
     
     # pt 2
+    pt2(read_input(test_input_file), True)
+    pt2(read_input(input_file))
     
     
 
